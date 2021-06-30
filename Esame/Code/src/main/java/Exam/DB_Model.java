4cd7f9070437efafd5fa15d5b2908aa3aa0be82a
nicorossi2810 <272179@studenti.unimore.it>
2021-06-30 16:59:04 +0200

    Aggiornamento per  DB

--- Esame/Code/src/main/java/Exam/DB_Model.java
+++ Esame/Code/src/main/java/Exam/DB_Model.java
@@ -1,6 +1,8 @@
 package Exam;
 
-import Exam.Utils.*;
+import Exam.Utils.DBManager;
+import Exam.Utils.Persona;
+import Exam.Utils.Utils;
 
 
 import java.sql.ResultSet;
@@ -8,50 +10,44 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Time;
 import java.time.LocalTime;
-
+// "com.mysql.cj.jdbc.Driver",
+//                "jdbc:mysql://localhost:3306/jdbc_prova?user=Nico&password=10"
 public class DB_Model {
-    ResultSet rs;
+     ResultSet rs;
     Statement statement;
-
-    public DB_Model() throws SQLException {
-        DBManager.setConnection(
-                 Utils.JDBC_Driver,
-                 Utils.JDBC_URL);
-        System.out.println("ciao2");
+    public DB_Model() throws SQLException{
+        DBManager.setConnection(Utils.JDBC_Driver,Utils.JDBC_URL);
         statement = DBManager.getConnection().createStatement();
-
-        try {
-            rs = statement.executeQuery("SELECT * FROM ATLETA");
-        } catch (SQLException e) {
+        try{
+            statement.executeQuery("SELECT * FROM ATLETA");
+        } catch (SQLException e){
             /* TOGLIERE ALLA FINE DEL PROGETTO */
             statement.executeQuery("DROP TABLE IF EXISTS atleta");
-            statement.executeQuery("CREATE TABLE ATLETA (" + " utente VARCHAR(50)," + " luogo_nascita VARCHAR(50)," + "data_nascita DATE" + "p_iva VARCHAR(50) NOT NULL UNIQUE" + "città_residenza VARCHAR(50)," + "CF VARCHAR(50)PRIMARY KEY," + " sport VARCHAR(50)" + "squadra VARCHAR(50))");
+            statement.executeQuery("CREATE TABLE ATLETA ("+" utente VARCHAR(50),"+" luogo_nascita VARCHAR(50),"+"data_nascita DATE"+"p_iva VARCHAR(50) NOT NULL UNIQUE"+"città_residenza VARCHAR(50),"+"CF VARCHAR(50)PRIMARY KEY,"+" sport VARCHAR(50)"+"squadra VARCHAR(50))");
             statement.executeQuery("INSERT INTO atleta (utente, luogo_nascita, data_nascita, p_iva, città_residenza, CF, sport, squadra) VALUES ('Piero Giovanni','Ladispoli','17/12/1995','0764352056C','Modena','FSGSGP47C22E764C','Tennis','Circolo Modena')");
             statement.executeQuery("INSERT INTO atleta (utente, luogo_nascita, città_residenza,data_nascita, p_iva, CF, sport) VALUES ('Cavoli Racho','Rodi','17/11/1991','0764357052C','Sassuolo','RNXCYH38D29G463W','Calcio','Valsa Calcio')");
             statement.executeQuery("INSERT INTO atleta (utente, luogo_nascita, città_residenza,data_nascita, p_iva, CF, sport) VALUES ('Giovanni Sforza','CentoCelle','14/02/2001','0764352086F','Reggio Emilia','NPBBVZ28P28I700E','Nuoto','Piscina Reggio Emilia')");
         }
     }
-
-    public Persona getSelectedItem() {
-        try {
-            return new Persona(rs.getString("Utente"), rs.getString("luogo_nascita"),
-                    rs.getDate("data_nascita"), rs.getString("p_iva"),
-                    rs.getString("città_residenza"), rs.getString("CF"), rs.getString("sport"));
-        } catch (SQLException e) {
+    public Persona getSelectedItem(){
+        try{
+            return new Persona(rs.getString("Utente"),rs.getString("luogo_nascita"),
+                    rs.getDate("data_nascita"),rs.getString("p_iva"),
+                    rs.getString("città_residenza"),rs.getString("CF"),rs.getString("sport"));
+        } catch (SQLException e){
             e.printStackTrace();
         }
         return null;
     }
 
-    public int getSelectedIndex() {
-        try {
+    public int getSelectedIndex(){
+        try{
             return rs.getRow();
-        } catch (SQLException e) {
+        }catch (SQLException e){
             e.printStackTrace();
         }
         return 0;
     }
-
     public void first() {
         try {
             rs.first();
@@ -85,21 +81,20 @@ public class DB_Model {
             e.printStackTrace();
         }
     }
-
-    public void insert(String utente, String luogo_nascita, String citta_residenza, String CF, LocalTime data_nascita, String sport, String squadra, String p_iva) {
-        try {
+   public void insert(String utente, String luogo_nascita, String citta_residenza, String CF, LocalTime data_nascita, String sport, String squadra, String p_iva){
+        try{
             rs.moveToInsertRow();
-            rs.updateString("utente", utente);
-            rs.updateString("luogo_nascita", luogo_nascita);
-            rs.updateString("citta_residenza", citta_residenza);
-            rs.updateString("CF", CF);
+            rs.updateString("utente",utente);
+            rs.updateString("luogo_nascita",luogo_nascita);
+            rs.updateString("citta_residenza",citta_residenza);
+            rs.updateString("CF",CF);
             rs.updateTime("data_nascita", Time.valueOf(data_nascita));
-            rs.updateString("sport", sport);
-            rs.updateString("squadra", squadra);
-            rs.updateString("p_iva", p_iva);
+            rs.updateString("sport",sport);
+            rs.updateString("squadra",squadra);
+            rs.updateString("p_iva",p_iva);
             rs.insertRow();
             rs.last();
-        } catch (SQLException e) {
+        } catch (SQLException e ){
             e.printStackTrace();
         }
     }
--- Esame/Code/src/main/java/Exam/Utils/DBManager.java
+++ Esame/Code/src/main/java/Exam/Utils/DBManager.java
@@ -1,19 +1,16 @@
 package Exam.Utils;
 
 import com.mysql.jdbc.Driver;
-import jdk.jshell.execution.Util;
 
-import java.io.File;
-import java.nio.ByteBuffer;
-import java.nio.file.Paths;
 import java.sql.*;
-import java.util.UUID;
 
 /*  DMManager Class for managing DB connections  */
 public class DBManager {
-        public static String JDBC_Driver = Utils.JDBC_Driver;
-        public static String JDBC_URL = Utils.JDBC_URL;
+
+        public static String JDBC_Driver = null ;
+        public static String JDBC_URL = null;
         static Connection connection;
+
 
         public static void setConnection(String Driver, String URL) {
             JDBC_Driver = Driver;
@@ -21,26 +18,31 @@ public class DBManager {
         }
 
         public static Connection getConnection() throws SQLException {
+
             if (connection == null) {
                 if (JDBC_Driver == null || JDBC_URL == null) {
                     throw new IllegalStateException("Illegal request. Call setConnection() before.");
                 }
                 try {
+
                     Class.forName(JDBC_Driver);
                 } catch (ClassNotFoundException e) {
                     e.printStackTrace();
                 }
+
                 connection = DriverManager.getConnection(JDBC_URL);
+                System.out.println("connected");
                 showMetadata();
             }
-            return connection;
 
+            return connection;
         }
 
         public static void showMetadata() throws SQLException {
             if (connection == null) {
                 throw new IllegalStateException("Illegal request. Connection not established");
             }
+
             DatabaseMetaData md = connection.getMetaData();
             System.out.println("-- ResultSet Type --");
             System.out.println("Supports TYPE_FORWARD_ONLY: " + md.supportsResultSetType(ResultSet.TYPE_FORWARD_ONLY));
@@ -56,6 +58,5 @@ public class DBManager {
                 connection.close();
             }
         }
-    }
 
-
+}
