a0aac03c44c2a7ea7a309f4b7d0ca70d659d7464
Laura Delsante <203829@studenti.unimore.it>
2021-06-30 17:07:14 +0200

    provo io

--- Esame/Code/src/main/java/Exam/SBM.java
+++ Esame/Code/src/main/java/Exam/SBM.java
@@ -1,13 +1,15 @@
 package Exam;
 
-
+//questo è il nostro progetto, madonne//
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.Serial;
 import java.sql.SQLException;
 
 public class SBM extends JFrame implements ActionListener {
-    private static final long serialVersionUID =1L;
+    @Serial
+    private static final long serialVersionUID = 1L;
     private static JMenu openArchivio;
     private static JMenuItem close;
     private static JMenuItem Atleti;
@@ -16,36 +18,35 @@ public class SBM extends JFrame implements ActionListener {
     private static JMenuItem Nuovo;
     private static JMenuItem Open;
     private static JFileChooser OpenSource;
-    int selectedItem;
     /*
     Creare una variabile model( come nell'esempio SM_Updatable < sausagemanager < jdbc per separare la grafica dai dati
      */
     DB_Model model;
 
-    public SBM(){
+    public SBM() {
         super("Sport Business Management ");
 
 
-        JMenuBar menuBar =new JMenuBar();
-        JMenu Archivio =new JMenu("Archivio");
+        JMenuBar menuBar = new JMenuBar();
+        JMenu Archivio = new JMenu("Archivio");
         menuBar.add(Archivio);
-        JMenu Rimborso =new JMenu("Rimborso");
-        JMenu Strutture =new JMenu("Strutture");
+        JMenu Rimborso = new JMenu("Rimborso");
+        JMenu Strutture = new JMenu("Strutture");
         menuBar.add(Rimborso);
         menuBar.add(Strutture);
 
-        openArchivio =new JMenu("Open Archivio di ...");
+        openArchivio = new JMenu("Open Archivio di ...");
         openArchivio.addSeparator();
         openArchivio.addActionListener(this);
         Archivio.add(openArchivio);
 
         //parte codice menu Archivio//
 
-        Atleti =new JMenuItem("Atleti");
+        Atleti = new JMenuItem("Atleti");
         Atleti.addActionListener(this);
         openArchivio.add(Atleti);
 
-        Allenatori =new JMenuItem("Allenatori");
+        Allenatori = new JMenuItem("Allenatori");
         Allenatori.addActionListener(this);
         openArchivio.add(Allenatori);
 
@@ -54,7 +55,7 @@ public class SBM extends JFrame implements ActionListener {
         openArchivio.add(Dirigenti);
 
         close = new JMenuItem("Close");
-         close.addActionListener(this);
+        close.addActionListener(this);
         Archivio.add(close);
 
         //parte codice menu Rimborso//
@@ -63,46 +64,43 @@ public class SBM extends JFrame implements ActionListener {
         Rimborso.add(Nuovo);
         //prova git//
 
-        Open=new JMenuItem("Apri...");
+        Open = new JMenuItem("Apri...");
         Open.addActionListener(this);
         Rimborso.add(Open);
 
-
-
         setJMenuBar(menuBar);
-        setSize(640,480);
+        setSize(640, 480);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
 
         try {
-           /* System.out.println(("ciao"));*/
+            /* System.out.println(("ciao"));*/
             model = new DB_Model();
             /*il problema è questo catch che non capisco perchè ci entro!!
             il resto ora funziona*/
-
-        } catch (SQLException | NullPointerException e ){
-            JOptionPane.showMessageDialog(this,"Errore nel Database!!");
+        } catch (SQLException e) {
+            /*System.out.println(e);*/
+            JOptionPane.showMessageDialog(this, "Database Error!");
         }
         // ShowItem(); come nel esempio del prof :  SM_Updatable < sausagemanager < jdbc
     }
 
-    public void actionPerformed(ActionEvent e){
+    public void actionPerformed(ActionEvent e) {
         if (e.getSource() == this.close) {
             dispose();
-        }
+        } else {
+            if (e.getSource() == this.Open) {
+                OpenSource = new JFileChooser();
 
+                /* int option = OpenSource.showOpenDialog(this);
+                if (option == JFileChooser.APPROVE_OPTION) {
 
-        else
-        {
-            if (e.getSource() == this.Open){
-                OpenSource =new JFileChooser();
-
-                 int option = OpenSource.showOpenDialog(this);
-                if (option == JFileChooser.APPROVE_OPTION){
-
-                 }
+                }*/
             }
+
+
         }
     }
+
 }
