3964470cd861f7051faf7205d9f2756683415e59
Laura Delsante <203829@studenti.unimore.it>
2021-08-26 17:09:46 +0200

    jdatepicker

--- Esame/Code/src/main/java/Exam/Rimborso.java
+++ Esame/Code/src/main/java/Exam/Rimborso.java
@@ -1,48 +1,92 @@
 package Exam;
 
 
+import Exam.Utils.DBManager;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.FileOutputStream;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Properties;
 
 
-public class Rimborso extends JPanel implements ActionListener {
-    public static String[] options = {"Ciclismo","Podismo","Calcio"};
+public class Rimborso extends JPanel implements ActionListener, KeyListener {
+    public static String[] options = {"Ciclismo", "Podismo", "Calcio"};
+
 
     JButton rim;
     JTextField days;
-    JTextField tcf= new JTextField();
-    JTextField tga1 =new JTextField();
-    JTextField tga2 =new JTextField();
-    JTextField tga3 =new JTextField();
-    JTextField tga4 =new JTextField();
-    JTextField tga5 =new JTextField();
-    JTextField tga6 =new JTextField();
-    JTextField tga7 =new JTextField();
-    JTextField tga8 =new JTextField();
+    JTextField tcf;
+    String datee;
+
     JComboBox<String> jc;
+    int numAll=8;
+    private JDatePickerImpl checkInDatePicker;
 
 
     public Rimborso() {
 
-        jc =new JComboBox(options);
-        JPanel p1 =new JPanel(new GridLayout(2,2));
+        days = new JTextField("");
+        tcf = new JTextField("");
+        tcf.addKeyListener(this);
+
+
+        setLayout(new BorderLayout());
+        jc = new JComboBox(options);
+        JPanel p1 = new JPanel(new GridLayout(1, 2));
         p1.add(new JLabel("CF"));
         p1.add(tcf);
-        JPanel p2 =new JPanel(new FlowLayout());
+        JPanel p2 = new JPanel(new GridLayout(1, 3));
         p2.add(jc);
         p2.add(new JLabel("Giorno Allenamento:"));
         p2.add(days);
 
-        setLayout(new BorderLayout());
-        add(p1,BorderLayout.PAGE_START);
 
+        JPanel p3 = new JPanel(new BorderLayout());
+        p3.add(p1, BorderLayout.PAGE_START);
+        p3.add(p2, BorderLayout.PAGE_END);
+
+        JPanel p4 = new JPanel(new BorderLayout());
+        Font font = new Font("Helvetica", Font.BOLD, 30);
+        JLabel titolo = new JLabel("Selezionare le date degli allenamenti:");
+        titolo.setFont(font);
+        p4.add(titolo, BorderLayout.NORTH);
+
+
+
+        JPanel grigliabox =new JPanel(new GridLayout(8,1));
+        for(int i =0; i< numAll; i++) {
+            UtilDateModel model = new UtilDateModel();
+            Properties properties = new Properties();
+            properties.put("text.today", "Today");
+            properties.put("text.month", "Month");
+            properties.put("text.year", "Year");
+            JDatePanelImpl datePanel = new JDatePanelImpl(model, properties);
+            checkInDatePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+
+            Date selectedDate = (Date) checkInDatePicker.getModel().getValue();
+            datee = selectedDate + "";
+            grigliabox.add(checkInDatePicker);
+        }
+        p4.add(grigliabox,BorderLayout.CENTER);
+
+        add(p3, BorderLayout.PAGE_START);
+        add(p4, BorderLayout.CENTER);
 
 
 
@@ -55,7 +99,7 @@ public class Rimborso extends JPanel implements ActionListener {
             PdfWriter.getInstance(document, new FileOutputStream(file_name));
 
             document.open();
-            Paragraph para =new Paragraph("Questa è una prova del file pdf_prova");
+            Paragraph para = new Paragraph("Questa è una prova del file pdf_prova");
             document.add(para);
             document.close();
 
@@ -68,9 +112,56 @@ public class Rimborso extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource()==this.rim){
+        if (e.getSource() == this.rim) {
             Generate_PDF();
         }
+    }
+
+
+    public void checkCF() throws SQLException {
+        Statement statement = DBManager.getConnection().createStatement();
+
+    }
+
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int pos = tcf.getCaretPosition();
+        tcf.setText(tcf.getText().toUpperCase());
+        tcf.setCaretPosition(pos);
+    }
+
+    public class DateLabelFormatter extends JFormattedTextField.AbstractFormatter {
+
+        private String datePattern = "dd/MM/yyyy";
+        private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
+
+        @Override
+        public Object stringToValue(String text) throws ParseException {
+            return dateFormatter.parseObject(text);
+        }
+
+        @Override
+        public String valueToString(Object value) throws ParseException {
+            if (value != null) {
+                Calendar cal = (Calendar) value;
+                return dateFormatter.format(cal.getTime());
+            }
+
+            return "";
+        }
+
     }
 }
 
